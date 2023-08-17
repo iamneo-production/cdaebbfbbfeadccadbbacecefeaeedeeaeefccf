e86f055a8801ee23822d898f9581596ba4cfe1f2
Iamneo <techadmin@iamneo.ai>
2023-08-17 07:01:43 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -1,11 +1,14 @@
 package com.example.springapp;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@con
-@SpringBootApplication
+@Controller
+@SpringBootApplicatio
 public class SpringappApplication {
-
+	@RequestMapping("/welcome")
+	@ResponseBody
 public String welcome()
 {
 	return "Welcome String Boot";
